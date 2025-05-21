3eb72dd8b85e2f2b196e276b7e7c1b9cc8c18c8c
NjabuloJ <fanajb65@gmail.com>
2025-05-21 11:57:49 +0200

    Create list.js

---
+++ commandes/list.js
@@ -0,0 +1,87 @@
+const util = require('util');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { fana } = require(__dirname + "/../njabulo/fana");
+const { format } = require(__dirname + "/../njabulo/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
+
+fana({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../njabulo/fana");
+
+    var commandsList = {};
+    var mode = (s.MODE).toLocaleLowerCase() !== "yes" ? "private" : "public";
+
+    cm.map((com) => {
+        if (!commandsList[com.categorie]) commandsList[com.categorie] = [];
+        commandsList[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('EAT');
+
+    const time = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    let infoMsg = `
+┊✺┌────••••────⊷
+┃✇│◎ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃✇│◎ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ]
+┃✇│◎ 𝙼𝚘𝚍𝚎 : ${mode}
+┃✇│◎ 𝙳𝚊𝚝𝚎  : ${date}
+\n`;
+
+    let menuMsg = ``;
+
+    for (const category in commandsList) {
+        menuMsg += `
+- . ${category} `;
+        for (const cmd of commandsList[category]) {
+            menuMsg += `          
+- . ${s.PREFIXE}  *${cmd}*`;
+        }
+        menuMsg += `
+- .`;
+    }
+
+    menuMsg += `.\n>`;
+
+    var imageUrl = mybotpic();
+
+    try {
+        if (imageUrl.match(/\.(mp4|gif)$/i)) {
+            await zk.sendMessage(dest, { video: { url: imageUrl }, caption: infoMsg + menuMsg, gifPlayback: true }, { quoted: ms });
+        } else if (imageUrl.match(/\.(jpeg|png|jpg)$/i)) {
+            await zk.sendMessage(dest, { image: { url: imageUrl }, caption: infoMsg + menuMsg }, { quoted: ms });
+        } else {
+            repondre(infoMsg + menuMsg);
+        }
+
+        // Download and send audio
+        const audioUrl = "https://files.catbox.moe/xci982.mp3";
+        const audioPath = "./temp_audio.mp3";
+
+        const response = await axios({
+            url: audioUrl,
+            method: "GET",
+            responseType: "stream",
+        });
+
+        const writer = fs.createWriteStream(audioPath);
+        response.data.pipe(writer);
+
+        writer.on("finish", async () => {
+            await zk.sendMessage(dest, { audio: { url: audioPath }, mimetype: "audio/mp4", ptt: true }, { quoted: ms });
+            fs.unlinkSync(audioPath); // Delete the audio file after sending
+        });
+
+    } catch (e) {
+        console.log("🥵🥵 Menu error: " + e);
+        repondre("🥵🥵 Menu error: " + e);
+    }
+});
+                  
